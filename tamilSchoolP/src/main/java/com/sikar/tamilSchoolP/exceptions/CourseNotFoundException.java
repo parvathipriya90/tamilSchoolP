5fce53cc9717a6f845a09eafa3363b128687edae
Parvathi Priya <mpaarvathi90@gmail.com>
2024-09-14 22:01:36 +0530

    removes course related information

--- tamilSchoolP/src/main/java/com/sikar/tamilSchoolP/exceptions/CourseNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.sikar.tamilSchoolP.exceptions;
-
-public class CourseNotFoundException extends RuntimeException{
-    public CourseNotFoundException(String message){
-        super(message);
-    }
-}
